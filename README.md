c621d0126611bfe71fc20bb4cdd4fa9f08e50332
SimMarkt <s.markth@posteo.de>
2025-03-25 11:56:04 +0100

    update 25032025

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
 # RL_Grid
 
+The RL_Grid project provides a grid world environment for analyzing maximization bias in Reinforcement Learning (RL). A common antidote to maximization bias is Double-Q-Learning. The present code includes Q-Learning and Double-Q-Learning in order to demonstrate the beneficial effect of learning multiple Q-functions. The grid world example 
+
+
+![RL_PtG_int](plots/GridWorld.png)
+
+*Figure 1: 2D grid world with the optimal path (s<sup>0</sup>, a<sup>e</sup>, s<sup>2</sup>, a<sup>e</sup>, T) [T: Terminal state; p(r|s,a): Probability of the reward r for taking action a in state s; q<sub>&pi,opt</sub>: State-action value of the optimal path in s<sup>0</sup>; q<sub>&pi,non-opt</sub>: Maximum state-action value of the non-optimal path in s<sup>0</sup>; &alpha: Learning rate; &gamma; Discount factor; &epsilon: Exploration coefficient].*
+
+
 ## Overview
 This project implements a reinforcement learning framework using a grid world environment. It includes various reinforcement learning agents and utilities for plotting results.
+
+
 
 ## Project Structure
 ```
--- src/rl_grid_utils.py
+++ src/rl_grid_utils.py
@@ -1,37 +1,42 @@
 # ---------------------------------------------------------------------------------------------
-# RL_Grid: Grid world for analyzing maximization bias in RL with Q-learning and Double Q-learning
+# RL_Grid: Grid world for analyzing maximization bias in RL using Q-learning and Double Q-learning
 #
 # rl_grid_utils.py:
-# > Utility functions for plotting the RL training results
+# > Utility functions for visualizing RL training results
 # ---------------------------------------------------------------------------------------------
 
 def plot_results(results):
     '''
-        Creates a plot with the Q-Values averaged over all runs and dependent on the training progress (# of episodes)
-        :param results: Dictionary with training results
+    Generates a plot showing the Q-values averaged over all runs as a function of training progress (number of episodes).
+    :param results: Dictionary containing training results
     '''
     import matplotlib.pyplot as plt
 
+    # Configure plot appearance
     plt.rcParams.update({'font.size': 14})
     plt.rcParams["font.family"] = "serif"
     plt.rcParams["font.serif"] = ["Times New Roman"]
     plt.rcParams['axes.linewidth'] = 1
 
+    # Create subplots for Q-Learning and Double Q-Learning
     fig, axs = plt.subplots(2, 1, figsize=(4, 5), sharex=True, sharey=False)
     
+    # Plot Q-Learning results
     axs[0].grid()
-    # axs[0].set_ylim([0, 0.8])
+    # axs[0].set_ylim([0, 0.8])  # Uncomment to set y-axis limits
     axs[0].plot(results["Q-Learning"][0], 'gray', linestyle="dotted", linewidth=1.7, label='Non-optimal')
     axs[0].plot(results["Q-Learning"][1], 'darkblue', linewidth=1.7, label='Optimal')
     axs[0].set_ylabel('State-action value q$_{\pi}$(s,a)')
 
+    # Plot Double Q-Learning results
     axs[1].grid()
-    # axs[1].set_ylim([0, 0.8])
+    # axs[1].set_ylim([0, 0.8])  # Uncomment to set y-axis limits
     axs[1].plot(results["DoubleQ-Learning"][0], 'gray', linestyle="dotted", linewidth=1.7, label='Non-optimal')
     axs[1].plot(results["DoubleQ-Learning"][1], 'darkblue', linewidth=1.7, label='Optimal')
     axs[1].set_ylabel('State-action value q$_{\pi}$(s,a)')
     axs[1].set_xlabel('Episodes')
 
+    # Add labels for the subplots
     textstr = 'Q-Learning'
     props = dict(boxstyle='Square', facecolor='white', alpha=0)
     axs[0].text(0.66, 1.05, textstr, fontsize=14, bbox=props, transform=axs[0].transAxes, verticalalignment='center')
@@ -40,12 +45,14 @@ def plot_results(results):
     props = dict(boxstyle='Square', facecolor='white', alpha=0)
     axs[1].text(0.5, 1.05, textstr, fontsize=14, bbox=props, transform=axs[1].transAxes, verticalalignment='center')
 
+    # Adjust subplot positions
     box = axs[0].get_position()
     axs[0].set_position([box.x0+0.05, box.y0, box.width, box.height])
 
     box = axs[1].get_position()
     axs[1].set_position([box.x0+0.05, box.y0, box.width, box.height])
 
+    # Add a legend to the plot
     legend = plt.legend(bbox_to_anchor=(0.04, 2.07, 0.55, 1), loc=3, ncol=1, mode="expand", borderaxespad=0., framealpha=1)
     frame = legend.get_frame()
     frame.set_facecolor('white')
